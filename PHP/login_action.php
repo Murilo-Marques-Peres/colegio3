d573c28916e40b8c51c54bc14c340bb955782c38
Murilo-Marques-Peres <muriloperes6@gmail.com>
2023-11-28 17:34:43 -0300

    First New Branch

--- PHP/login_action.php
+++ PHP/login_action.php
@@ -12,9 +12,24 @@ $sql = $pdo->prepare("SELECT * FROM login WHERE usuario = :usuario AND senha = :
 $sql->bindValue(':usuario', $usuario);
 $sql->bindValue(':senha', $senha);
 $sql->execute();
+
+$info = $sql->fetchAll();
+foreach($info as $key => $element){
+    $tipo = $element["tipo"];
+}
+
+
+
+
 if($usuario && $senha){
-    if($sql->rowCount() > 0){
+    if($sql->rowCount() > 0 && $tipo == "Aluno"){
         header("Location: pageNotas");
+        exit;
+    }else{
+        header("location: ../index");
+    }
+    if($sql->rowCount() > 0 && $tipo == "Admin"){
+        header("Location: pageAdmin");
         exit;
     }else{
         header("location: ../index");
